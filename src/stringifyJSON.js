91156d76f3d58bc6fcb303c66a4223a44eef9e85
Vivian Chen <vichen@vassar.edu>
2016-01-25 18:37:35 -0700

    attempt at stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(objecty) {
+  if (Array.isArray(objecty)) {
+      function arrayHelper(array, index) {
+          var newArray = [];
+          if (index==(array.length)) {
+              return newArray;
+          } else if (typeof array[index] == ("undefined" || "function")) {
+              return arrayHelper(array.slice(index), index+1);
+          } else {
+              newArray.push(JSON.stringify(array[index]));
+              return arrayHelper(array.slice(index), index+1);
+          }
+      }
+      return arrayHelper(objecty, 0);
+  } else if (objecty instanceof Object) {
+      function objHelper(obj, index) {
+          var objKeys = Object.keys(obj);
+          var newObj = {};
+          if (index == (objKeys.length)) {
+              return newObj;
+          } else if (typeof objKeys[index] == ("undefined" || "function")) {
+              return objHelper(obj, index+1);
+          } else {
+              newObj[JSON.stringify(objKeys[index])] = 
+                  JSON.stringify(obj[objKeys[index]]);
+              return objHelper(obj, index+1);
+          }
+      }
+      return objHelper(objecty, 0);
+  } else {
+      return JSON.stringify(objecty);
+  }
 };

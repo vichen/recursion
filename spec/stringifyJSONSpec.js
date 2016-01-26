821a15d4ad68a954ad2265ee9b2477fdec9e0953
Vivian Chen <vichen@vassar.edu>
2016-01-25 21:41:12 -0700

    stringifyJSON completed

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -5,17 +5,12 @@ describe('stringifyJSON', function(){
     stringifiableObjects.forEach(function(test){
       var expected = JSON.stringify(test);
       var result = stringifyJSON(test);
-        console.log(Array.isArray(stringifiableObjects));
-        console.log("stringifiable expected: " + expected);
-        console.log("stringifiable result: " + result);
       expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj){
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-        console.log("unstringifiable expected: " + expected);
-        console.log("unstringifiable result: " + result);
       expect(result).to.equal(expected);
     });
 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,36 +3,54 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(objecty) {
-  if (Array.isArray(objecty)) {
-      function arrayHelper(array, index) {
-          var newArray = [];
-          if (index==(array.length)) {
-              return newArray;
-          } else if (typeof array[index] == ("undefined" || "function")) {
-              return arrayHelper(array.slice(index), index+1);
-          } else {
-              newArray.push(JSON.stringify(array[index]));
-              return arrayHelper(array.slice(index), index+1);
-          }
-      }
-      return arrayHelper(objecty, 0);
-  } else if (objecty instanceof Object) {
-      function objHelper(obj, index) {
-          var objKeys = Object.keys(obj);
-          var newObj = {};
-          if (index == (objKeys.length)) {
-              return newObj;
-          } else if (typeof objKeys[index] == ("undefined" || "function")) {
-              return objHelper(obj, index+1);
-          } else {
-              newObj[JSON.stringify(objKeys[index])] = 
-                  JSON.stringify(obj[objKeys[index]]);
-              return objHelper(obj, index+1);
-          }
-      }
-      return objHelper(objecty, 0);
-  } else {
-      return JSON.stringify(objecty);
-  }
+var stringifyJSON = function (objecty) {
+    var CURRENT = 0;
+    var NEXT = 1;
+
+    if (objecty === null || objecty === undefined) {
+        return "null"; // return nothing
+    }
+    if (Array.isArray(objecty)) {
+        function arrayHelper(array) {
+
+            if (array.length === 0) {
+                return '';
+            } else if (typeof array[CURRENT] === 'undefined' || typeof array[CURRENT] === 'function') {
+                var str = 'null';
+                if (array[NEXT] !== undefined) {
+                    str += ',';
+                }
+                return str + arrayHelper(array.slice(NEXT));
+            } else {
+                var str = JSON.stringify(array[CURRENT]);
+                if (array[NEXT] !== undefined) {
+                    str += ',';
+                }
+                return str + arrayHelper(array.slice(NEXT));
+            }
+        }
+        return '[' + arrayHelper(objecty) + ']';
+    } else if (typeof objecty === 'string' || typeof objecty === 'number' || typeof objecty === 'boolean') {
+        return JSON.stringify(objecty);
+    } else if (typeof objecty === 'object') {
+        var objKeys = Object.keys(objecty);
+
+        function objHelper(keys) {
+            if (keys.length === 0) {
+                return '';
+            } else if (typeof keys[CURRENT] === 'undefined' ||
+                typeof keys[CURRENT] === 'function' ||
+                typeof objecty[keys[CURRENT]] === 'undefined' ||
+                typeof objecty[keys[CURRENT]] === 'function') {
+                return objHelper(keys.slice(NEXT));
+            } else {
+                var str = JSON.stringify(objecty[keys[CURRENT]]);
+                if (keys[NEXT] !== undefined) {
+                    str += ',';
+                }
+                return JSON.stringify(keys[CURRENT]) + ":" + str + objHelper(keys.slice(NEXT));
+            }
+        }
+        return '{' + objHelper(objKeys) + '}';
+    }
 };
